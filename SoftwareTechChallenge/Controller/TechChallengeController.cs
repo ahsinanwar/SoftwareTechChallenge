418ce99723f549a41bc656c4067f1d2477929a03
ahsinanwar <ahsinanwar@gmail.com>
2020-02-19 23:31:19 +0500

    seconf push

---
+++ SoftwareTechChallenge/Controller/TechChallengeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SoftwareTechChallenge.Helper;
+
+namespace SoftwareTechChallenge.Controller
+{
+    /// <summary>
+    /// This is an API Controller which is used to take input from clients and send response to client after checking either the input is multiple of 3 or 5
+    /// </summary>
+    public class TechChallengeController : ApiController
+    {
+        /// <summary>
+        /// This method get input from client and use MultipleCheckerHelper class to process
+        /// </summary>
+        /// <param name="id">Input from client</param>
+        /// <returns>It return PE if input is multiple of 3 and 5, return E if input is multiple of 5, return P if input is multiple of 5, return input as string if number is not a multilpe of 3 and 5   </returns>
+        public IHttpActionResult Get(int? id)
+        {
+            // Create an instance of MultipleCheckerHelper
+            MultipleCheckerHelper multipleCheckerHelper = new MultipleCheckerHelper();
+            string returnMessage = "";
+            // if client did not send input while requesting the API method, system send incorrect format message
+            if (id==null)
+                returnMessage = "Input is not in correct format";
+            else
+            {
+                if (multipleCheckerHelper.IsMultipleOfFiveAndThree((int)id))
+                    returnMessage = "PE";
+                else if (multipleCheckerHelper.IsMultipleOfNumber((int)id,5))
+                    returnMessage = "E";
+                else if (multipleCheckerHelper.IsMultipleOfNumber((int)id,3))
+                    returnMessage = "P";
+                else
+                    returnMessage = id.Value.ToString();
+
+            }
+            return Json(returnMessage);
+        }
+    }
+}
---
+++ SoftwareTechChallenge/Helper/MultipleCheckerHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftwareTechChallenge.Helper
+{
+    public class MultipleCheckerHelper
+    {
+        /// <summary>
+        /// This method checks the input, if input is multiple of 3 and 5 then it return true
+        /// </summary>
+        /// <param name="input">input from API method which is an integer number</param>
+        /// <returns>return true if input is multiple of 3 and 5</returns>
+        public bool IsMultipleOfFiveAndThree(int input)
+        {
+            if (input % 3 == 0 && input % 5 == 0)
+                return true;
+            else
+                return false;
+        }
+        /// <summary>
+        /// This method checks the input, if input is multiple of provided number
+        /// </summary>
+        /// <param name="input">input from API method which is an integer number</param>
+        /// <param name="multiple">multiple is an integer number which will be used as a multiplier</param>
+        /// <returns>return true if input is multiple of provided multiple parameter</returns>
+        public bool IsMultipleOfNumber(int input,int multiple)
+        {
+            if (input % multiple == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
